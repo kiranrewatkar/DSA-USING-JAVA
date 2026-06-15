449be32021ee7cc8471ed2397b4970a984c5d58f
Kiran Rewatkar <kiranrewatkar2004@gmail.com>
2026-06-15 21:49:26 +0530

    pattern continued

--- Basics/Main.java
+++ Basics/Main.java
@@ -524,5 +524,97 @@
 ?       *
 ?     * * *
 ?   * * * * *
-? * * * * * * *
+?     * * *
+?       * 
 */
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter no: "); //Enter--> 5
+//         int n = sc.nextInt();
+
+//         int space = n /2;
+//         int star = 1;
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= space; j++) {
+//                 System.out.print("\t");
+//             }
+
+//             for (int j = 1; j <= star; j++) {
+//                 System.out.print("*\t");
+//             }
+
+//             if (i <= n/2) {
+//                 space--;
+//                 star += 2;
+//             }
+//             else{
+//                 space++;
+//                 star -= 2;
+//             }
+//             System.out.println();
+//         }
+        
+
+//         sc.close();
+//     }
+// }
+//---------------------------------------------------
+/*
+? *       *       *       *       *
+? *       *       *       *       *
+? *       *       *       *       *
+? *       *       *       *       *
+? *       *       *       *       *
+*/
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for(int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 System.out.print5
+//                 ("*\t");
+//             }
+//             System.out.println();
+//         }
+        
+
+//         sc.close();
+//     }
+// }
+//---------------------------------------------------
+/*
+? *                               *
+?         *               *
+?                 *
+?         *               *
+? *                               *
+*/
+
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for(int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 if(i == j || i + j == n + 1) {
+//                 System.out.print("*\t");
+//             }
+//             else {
+//                 System.out.print("\t");
+//             }
+//             }
+//             System.out.println();
+//         }
+        
+
+//         sc.close();
+//     }
+// }
